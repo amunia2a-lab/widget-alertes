418a21ff1dd3616f69dcb5878491a7776f110f41
amunia2a-lab <amunia2a@gmail.com>
2026-03-13 11:39:10 +0100

    Delete netlify/functions directory

--- netlify/functions/resolve.js
+++
@@ -1,1 +0,0 @@
-export default async (req)=>{const b=JSON.parse(req.body);await fetch(`https://api.notion.com/v1/pages/${b.id}`,{method:'PATCH',headers:{Authorization:`Bearer ${process.env.NOTION_API_KEY}`,'Notion-Version':'2022-06-28','Content-Type':'application/json'},body:JSON.stringify({properties:{Statut:{select:{name:'Réglé'}}}})});return new Response('{}')}
